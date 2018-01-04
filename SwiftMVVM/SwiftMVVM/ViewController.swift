d3cb0ad9fc9b8c94fbb257e379f663eee7faf218
Radi <radishikerov@yahoo.com>
2018-01-04 10:56:56 +0200

    Create swift app to implement mvvm pattern

---
+++ SwiftMVVM/SwiftMVVM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftMVVM
+//
+//  Created by Radi Shikerov on 4.01.18.
+//  Copyright © 2018 Radi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
