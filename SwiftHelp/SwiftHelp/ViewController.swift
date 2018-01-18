8496a6d515fdee62598d3f54e951576c6631938e
Radi <radishikerov@yahoo.com>
2018-01-18 10:46:00 +0200

    Show masked image depending on percentage

--- SwiftHelp/SwiftHelp/ViewController.swift
+++ SwiftHelp/SwiftHelp/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ivGradient: UIImageView!
+    var maskView : UIView!
+    var maskLayer : CALayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        maskView = UIView(frame: CGRect(x: 0,
+                                        y: 0,
+                                        width: self.ivGradient.bounds.width,
+                                        height: 0));
+        maskView.backgroundColor = UIColor.white.withAlphaComponent(0.8);
+        self.ivGradient.addSubview(maskView);
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onPercentageChange(_ sender: Any) {
+        let precentage = 1 - Double((sender as! UIButton).tag)/100.0;
+        self.addSublayer(percentage: precentage);
     }
-
-
+    
+    func addSublayer(percentage : Double) -> Void {
+        let maskHeight = Double(self.ivGradient.bounds.height) * percentage;
+        maskView.frame = CGRect(x: 0.0,
+                                y: Double(self.ivGradient.bounds.height) - maskHeight,
+                                width: Double(self.ivGradient.bounds.width),
+                                height: maskHeight);
+    }
 }
 
