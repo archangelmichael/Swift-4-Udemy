218f3e6de7273f098b506cbbbe6f8246abaacffa
Radi <radishikerov@yahoo.com>
2018-01-24 23:56:02 +0200

    Implement custom round view and round text field.

---
+++ SwiftHelp/SwiftHelp/Views/RoundTextField.swift
@@ -0,0 +1,38 @@
+//
+//  RoundTextField.swift
+//  SwiftHelp
+//
+//  Created by Radi Shikerov on 24.01.18.
+//  Copyright © 2018 Radi. All rights reserved.
+//
+
+import UIKit
+
+class RoundTextField: UITextField {
+
+    @IBInspectable var cornerRadius : CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth : CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor : UIColor? {
+        didSet {
+            self.layer.borderColor = borderColor?.cgColor
+        }
+    }
+
+    @IBInspectable var placeholderColor: UIColor? {
+        didSet {
+            let placeholderString = self.attributedPlaceholder?.string != nil ? attributedPlaceholder!.string : ""
+            let attrPlaceholder = NSAttributedString(string: placeholderString, attributes: [NSAttributedStringKey.foregroundColor: placeholderColor!])
+            self.attributedPlaceholder = attrPlaceholder
+        }
+    }
+}
---
+++ SwiftHelp/SwiftHelp/Views/RoundView.swift
@@ -0,0 +1,31 @@
+//
+//  RoundView.swift
+//  SwiftHelp
+//
+//  Created by Radi Shikerov on 24.01.18.
+//  Copyright © 2018 Radi. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundView: UIView {
+
+    @IBInspectable var cornerRadius : CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth : CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor : UIColor? {
+        didSet {
+            self.layer.borderColor = borderColor?.cgColor
+        }
+    }
+}
